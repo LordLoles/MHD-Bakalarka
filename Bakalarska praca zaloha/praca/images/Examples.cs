631237109936083768647017148e4f7824471de6
LordLoles <alojzstupal@gmail.com>
2018-05-13 15:48:00 +0200

    skoro cela implementacia

--- Bakalarska praca zaloha/praca/images/Examples.cs
+++ Bakalarska praca zaloha/praca/images/Examples.cs
@@ -14,6 +14,7 @@ public class Vertex
     }
 }
 
+public class Time { }
 
 
 public class Edge
@@ -21,15 +22,19 @@ public class Edge
     internal string name;
     internal Vertex fromV;
     internal Vertex toV;
+    internal Time fromT;
+    internal Time toT;
     internal int travellTime; //minutes
 
 
-    public Edge(string name, Vertex fromV, Vertex toV, int travellTime)
+    public Edge(string name, Vertex fromV, Vertex toV, Time fromT, Time toT)
     {
         this.name = name;
         this.fromV = fromV;
         this.toV = toV;
-        this.travellTime = travellTime;
+        this.fromT = fromT;
+        this.toT = toT;
+        this.travellTime = Time.differenceBetweenTimesMin(this.fromT, this.toT);
     }
 }
 
@@ -44,5 +49,24 @@ public class Graph
     {
         verteces = new List<Vertex>();
         edges = new List<Edge>();
+    }
+
+}
+
+
+public class Vertex
+{
+    internal string name;
+    internal Time time;
+
+    internal int value; //for dijkstra's purpose
+    internal Vertex parent; //for dijkstra's purpose
+    internal Edge toParent; //for dijkstra's purpose
+
+
+    public Vertex(string n, Time t)
+    {
+        name = n;
+        time = t;
     }
 }

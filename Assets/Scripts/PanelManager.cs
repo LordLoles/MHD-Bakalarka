3d1aa34549d4ace03fd1127b7265a17fa8b54231
LordLoles <alojzstupal@gmail.com>
2018-05-14 14:47:20 +0200

    urobene zobrazovanie panela

---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour {
+
+    public GameObject zastavkaObj;
+    public GameObject linkaObj;
+    public GameObject scrollPanelObj;
+
+    private GameObject scrollPanel;
+    private int step = 100;
+    private int now = 0;
+
+    private void Awake()
+    {
+        scrollPanel = Instantiate(scrollPanelObj, transform) as GameObject;
+    }
+
+    public GameObject addStop(Vertex v)
+    {
+        GameObject go = Instantiate(zastavkaObj, scrollPanel.transform);
+        correctPos(go);
+        return go;
+    }
+
+
+    public GameObject addLink(Edge e)
+    {
+        GameObject go = Instantiate(linkaObj, scrollPanel.transform);
+        correctPos(go);
+        return go;
+    }
+
+
+    private void correctPos(GameObject go)
+    {
+        go.transform.localPosition = new Vector3(now, 0, 0);
+        now += step;
+    }
+
+    public void correctNameTime(GameObject go)
+    {
+        
+    }
+
+}
--- Assets/Scripts/PathShowing.cs
+++ Assets/Scripts/PathShowing.cs
@@ -3,22 +3,57 @@ using System.Collections.Generic;
 
 public class PathShowing : MonoBehaviour
 {
+    public GameObject panelObj;
+    public GameObject canvas;
     
-    
+    private int panelStep = -120;
+    private int now = -170;
+    private List<GameObject> panels;
+
+
+    private void Start()
+    {
+        panels = new List<GameObject>();
+        List<Edge> l = new List<Edge>();
+
+        l.Add(new Edge("e", new Vertex("v1", new Time(0, 0)), new Vertex("v2", new Time(0, 5)), new Time(0, 0), new Time(0, 5)));
+        printPath(l);
+    }
+
     public void printPath(List<Edge> path)
     {
+        GameObject panel = Instantiate(panelObj, canvas.transform);
+        correctPanelPos(panel);
+        panels.Add(panel);
+        PanelManager panelManager = panel.GetComponent<PanelManager>();
+
         for (int i = 0; i < path.Count; i++)
         {
-            //vytvor panel
-            
-            //if (i==0) vytvor dve zastavky a hranu medzi
-            //else hranu a zastavku
+            Edge e = path[i];
+            if (i == 0)
+            {
+                panelManager.addStop(e.fromV);
+                panelManager.addLink(e);
+                panelManager.addStop(e.toV);
+            }
+            else
+            {
+                panelManager.addLink(e);
+                panelManager.addStop(e.toV);
+            }
         }
     }
 
     public void printThis(Edge e)
     {
         Debug.Log(e.ToString());
+    }
+    
+
+    private void correctPanelPos(GameObject go)
+    {
+        go.transform.localPosition = new Vector3(0, now, 0);
+        now += panelStep;
     }
 
 
